fd91004c35b35c17c0fd11dd50b24d66e4da3601
Phdxmy336 <63434281+Phdxmy336@users.noreply.github.com>
2026-05-23 16:03:38 +0800

    更新 request-body.js

--- request-body.js
+++ request-body.js
@@ -1,6 +1,13 @@
-function syncCreditHoursToTotalTime(requestBody) {
-  return {
-    ...requestBody,
-    creditHours: requestBody.totalTime
-  };
-}
+// 拦截请求体，修改 creditHours 为 totalTime 的值
+(function() {
+    var bodyString = $request.body;
+    if (!bodyString) return;
+    
+    var body = JSON.parse(bodyString);
+    
+    if (body.totalTime != null && body.creditHours != null) {
+        body.creditHours = body.totalTime;
+    }
+    
+    $done({ body: JSON.stringify(body) });
+})();
