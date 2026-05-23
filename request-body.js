b846bfe1aaf27db750c64ff3f910a02bd17a07cf
Phdxmy336 <63434281+Phdxmy336@users.noreply.github.com>
2026-05-23 16:08:04 +0800

    更新 request-body.js

--- request-body.js
+++ request-body.js
@@ -1,13 +1,26 @@
-// 拦截请求体，修改 creditHours 为 totalTime 的值
-(function() {
-    var bodyString = $request.body;
-    if (!bodyString) return;
-    
-    var body = JSON.parse(bodyString);
-    
-    if (body.totalTime != null && body.creditHours != null) {
-        body.creditHours = body.totalTime;
-    }
-    
-    $done({ body: JSON.stringify(body) });
-})();
+// 创建一个代理函数来修改请求体
+function createModifiedRequestBody(originalBody) {
+  // 深拷贝原始对象
+  const modifiedBody = JSON.parse(JSON.stringify(originalBody));
+  
+  // 将 creditHours 修改为 totalTime 的值
+  if (modifiedBody.hasOwnProperty('totalTime') && modifiedBody.hasOwnProperty('creditHours')) {
+    modifiedBody.creditHours = modifiedBody.totalTime;
+  }
+  
+  return modifiedBody;
+}
+
+// 使用方式：
+const requestBody = {
+  "userName": "xxx",
+  "courseHour": 5,
+  "creditHours": 21,
+  "educationTrainingDtlId": "2026052206034525186016",
+  "userCode": "68xxxx",
+  "safetyStudyMstId": "20260521144141366757760",
+  "totalTime": 300
+};
+
+// 修改后的请求体
+const modifiedBody = createModifiedRequestBody(requestBody);
